6d3448a91d33e2a18060486feb890a07bc9edbeb
Timo Reitz <godsboss@gmx.de>
2018-06-27 20:28:00 +0200

    Add CLI wrappers for agent and server.

---
+++ backend/cli/wrap/agent.go
@@ -0,0 +1,25 @@
+package cli
+
+import (
+	"github.com/juzempelde/procwatch/backend/agent"
+	"github.com/juzempelde/procwatch/backend/cli"
+)
+
+// Agent wraps an agent into something the CLI package can use.
+func Agent(ag *agent.Agent) cli.Agent {
+	return &cliAgent{
+		agent: ag,
+	}
+}
+
+type cliAgent struct {
+	agent *agent.Agent
+}
+
+func (ag *cliAgent) Exec() error {
+	return ag.Exec()
+}
+
+func (ag *cliAgent) SetServerRPCAddr(addr string) {
+	ag.agent.ServerRPCAddr = addr
+}
---
+++ backend/cli/wrap/server.go
@@ -0,0 +1,25 @@
+package cli
+
+import (
+	"github.com/juzempelde/procwatch/backend/cli"
+	"github.com/juzempelde/procwatch/backend/server"
+)
+
+// Server wraps a server into something the CLI package can use.
+func Server(srv *server.Server) cli.Server {
+	return &cliServer{
+		server: &server.Server{},
+	}
+}
+
+type cliServer struct {
+	server *server.Server
+}
+
+func (srv *cliServer) Exec() error {
+	return srv.server.Run()
+}
+
+func (srv *cliServer) SetAddr(addr string) {
+	srv.server.RPCAddr = addr
+}
