1b369b731c7aa8befaebdfe2ea8f03d22b5276f2
Timo Reitz <godsboss@gmx.de>
2018-07-01 20:55:17 +0200

    Enhance server.

--- backend/server/server.go
+++ backend/server/server.go
@@ -11,18 +11,31 @@ import (
 type Server struct {
 	RPCAddr string
 
+	listener net.Listener
+
 	Devices Devices
 }
 
 // Run starts the RPC server.
 func (server *Server) Run() error {
+	rpcServer := server.createRPCServer()
 	listener, err := net.Listen("tcp", server.RPCAddr)
 	if err != nil {
 		return err
 	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			// What to do here?
+			continue
+		}
+		go rpcServer.ServeConn(conn) // TODO: Shutdown
+	}
+}
+
+func (server *Server) createRPCServer() *rpc.Server {
 	rpcServer := rpc.NewServer()
-	rpcServer.Accept(listener)
-	return nil
+	return rpcServer
 }
 
 type Devices interface {
