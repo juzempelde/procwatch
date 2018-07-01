4c8bcc11180c4c2665e088196a5276de608efb31
Timo Reitz <godsboss@gmx.de>
2018-07-01 22:37:06 +0200

    Add Device mock.

---
+++ mock/device.go
@@ -0,0 +1,21 @@
+package mock
+
+import (
+	"github.com/juzempelde/procwatch/backend"
+)
+
+type Device struct {
+	DisconnectError error
+	IdentifyErrors  map[procwatch.DeviceID]error
+}
+
+func (dev *Device) Identify(id procwatch.DeviceID) error {
+	if dev.IdentifyErrors == nil {
+		return nil
+	}
+	return dev.IdentifyErrors[id]
+}
+
+func (dev *Device) Disconnect() error {
+	return dev.DisconnectError
+}
