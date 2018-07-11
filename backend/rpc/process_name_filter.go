7abe36e539241b5f267f1ad4f629f98e41928fcd
Timo Reitz <godsboss@gmx.de>
2018-07-11 19:19:43 +0200

    Add RPC type for process name filter.

---
+++ backend/rpc/process_name_filter.go
@@ -0,0 +1,16 @@
+package rpc
+
+type ProcessNameFilter struct {
+	Names []string
+}
+
+func (procs *ProcessNameFilter) Expose(request ProcessNameFilterRequest, response *ProcessNameFilterResponse) error {
+	response.Names = procs.Names
+	return nil
+}
+
+type ProcessNameFilterRequest struct{}
+
+type ProcessNameFilterResponse struct {
+	Names []string
+}
