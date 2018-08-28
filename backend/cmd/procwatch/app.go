92d71c1fe50a4a1b36c9c5c65d2a7d89038abef6
Timo Reitz <godsboss@gmx.de>
2018-08-28 20:12:40 +0200

    Move ps package into agent.

--- backend/cmd/procwatch/app.go
+++ backend/cmd/procwatch/app.go
@@ -3,9 +3,9 @@ package main
 import (
 	"github.com/juzempelde/procwatch/backend"
 	"github.com/juzempelde/procwatch/backend/agent"
+	"github.com/juzempelde/procwatch/backend/agent/ps"
 	"github.com/juzempelde/procwatch/backend/cli"
 	"github.com/juzempelde/procwatch/backend/cli/wrap"
-	"github.com/juzempelde/procwatch/backend/ps"
 	"github.com/juzempelde/procwatch/backend/server"
 
 	"os"
